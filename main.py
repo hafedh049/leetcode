c818f8db5e261a1b074fa2207d71bea6fcf763dc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-23 23:39:20 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ from typing import List
 def maximum_length_of_a_concatenated_string_with_unique_characters(
     arr: List[str],
 ) -> int:
+    arr = [item for item in arr if len(item) == len(set(item))]
     if arr == []:
         return 0
     if len(arr) == 1 and len(set(arr[0])) == len(arr[0]):
         return len(arr[0])
-    arr.sort()
     result = 0
     for i in range(len(arr)):
-        ch = arr[i]
         for j in range(i + 1, len(arr)):
-            if len(set(ch + arr[j])) == len(ch + arr[j]):
-                ch += arr[j]
+            if len(set(arr[i] + arr[j])) == len(ch + arr[j]):
+                ch = arr[i] + arr[j]
             result = max(result, len(ch))
     return result
 
 
 print(
     maximum_length_of_a_concatenated_string_with_unique_characters(
-        ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+        ["a", "abc", "d", "de", "def"]
     )
 )
