750280094d610b3c8f42e4a02c7c9742f3a98246
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-23 23:29:20 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
-
 from typing import List
 
 
-def maximum_length_of_a_concatenated_string_with_unique_characters(arr : List[str])->int:
+def maximum_length_of_a_concatenated_string_with_unique_characters(
+    arr: List[str],
+) -> int:
     if arr == []:
         return 0
     if len(arr) == 1 and len(set(arr[0])) == len(arr[0]):
@@ -11,8 +11,16 @@ def maximum_length_of_a_concatenated_string_with_unique_characters(arr : List[st
     arr.sort()
     result = 0
     for i in range(len(arr)):
-        for j in range(i+1, len(arr) - 1):
-            ...
+        ch = arr[i]
+        for j in range(i + 1, len(arr)):
+            if len(set(ch + arr[j])) == len(ch + arr[j]):
+                ch += arr[j]
+            result = max(result, len(ch))
     return result
 
-print(maximum_length_of_a_concatenated_string_with_unique_characters(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]))
+
+print(
+    maximum_length_of_a_concatenated_string_with_unique_characters(
+        ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]
+    )
+)
