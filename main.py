5b00d97068eab0c6ad77dfb55632991aeddc4d54
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-23 23:19:20 +0100

    ModifiedM main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 
 
+from typing import List
+
+
 def maximum_length_of_a_concatenated_string_with_unique_characters(arr : List[str])->int:
-    ...
+    if arr == []:
+        return 0
+    if len(arr) == 1 and len(set(arr[0])) == len(arr[0]):
+        return len(arr[0])
+    arr.sort()
+    result = 0
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr) - 1):
+            ...
+    return result
+
+print(maximum_length_of_a_concatenated_string_with_unique_characters(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]))
